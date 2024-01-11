fc0c557c4c2fcae00ab258e38b23787924652291
a <a@b.c>
2024-01-11 21:48:53 +0700

    отключение регистро-независимой (но локале-зависимой) сортировки меток

--- src/libs/tagging.c
+++ src/libs/tagging.c
@@ -2665,18 +2665,19 @@ static gint _sort_tree_count_func(GtkTreeModel *model, GtkTreeIter *a, GtkTreeIt
 
 static inline gint _compare_utf8_no_case(const char *a, const char *b)
 {
-  char *a_nc = g_utf8_casefold(a, -1);
-  char *a_nc_nat = g_utf8_collate_key_for_filename(a_nc, -1);
-  g_free(a_nc);
+  //ab char *a_nc = g_utf8_casefold(a, -1);
+  //ab char *a_nc_nat = g_utf8_collate_key_for_filename(a_nc, -1);
+  //ab g_free(a_nc);
 
-  char *b_nc = g_utf8_casefold(b, -1);
-  char *b_nc_nat = g_utf8_collate_key_for_filename(b_nc, -1);
-  g_free(b_nc);
+  //ab char *b_nc = g_utf8_casefold(b, -1);
+  //ab char *b_nc_nat = g_utf8_collate_key_for_filename(b_nc, -1);
+  //ab g_free(b_nc);
 
-  const gint sort = g_strcmp0(a_nc_nat, b_nc_nat);
+  //ab const gint sort = g_strcmp0(a_nc_nat, b_nc_nat);
+  const gint sort = g_strcmp0(a, b); //ab
 
-  g_free(a_nc_nat);
-  g_free(b_nc_nat);
+  //ab g_free(a_nc_nat);
+  //ab g_free(b_nc_nat);
   return sort;
 }
 
