97e20479e07bde053be288ab4f948da10769cd0d
esq4 <esq654@gmail.com>
2025-10-25 20:41:54 +0700

    dt-cli: use metadata.flags from conf

--- src/cli/main.c
+++ src/cli/main.c
@@ -812,6 +812,12 @@ int main(int argc, char *arg[])
     dt_export_metadata_t metadata;
     metadata.flags = dt_lib_export_metadata_default_flags();
     metadata.list = NULL;
+    if (custom_presets)
+    {
+      metadata.flags = dt_lib_export_metadata_get_conf_flags();
+      metadata.list = dt_util_str_to_glist("\1", dt_lib_export_metadata_get_conf());
+      metadata.list = g_list_remove(metadata.list, metadata.list->data);
+    }
     if(storage->store(storage, sdata, id, format, fdata, num, total, high_quality,
                       upscale, FALSE, 1.0, export_masks,
                       icc_type, icc_filename, icc_intent, &metadata) != 0)
